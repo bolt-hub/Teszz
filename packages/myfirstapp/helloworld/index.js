527b28df19daabc3bf8431a9530caaa1c2a62b66
boltathi <athithya24@gmail.com>
2021-02-07 20:23:00 +0530

    dsa

--- packages/myfirstapp/helloworld/index.js
+++ packages/myfirstapp/helloworld/index.js
@@ -1,5 +1,5 @@
 async function main(args) {
-    return { body: "hello world  ! Node env is "+process.env.NODE_ENV };
+    return { body: "hd  ! Node env is "+process.env.NODE_ENV };
 }
 
 module.exports =  {main}
